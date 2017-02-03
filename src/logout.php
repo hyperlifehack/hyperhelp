b90af7a1ed18608efe33051b6228ffb24f13c1fc
username <sellerie@tutanota.com>
2017-02-03 01:43:53 +0100

    logout improved

--- src/logout.php
+++ src/logout.php
@@ -17,14 +17,18 @@ include('text/head.php');
 			<div id="content">
 				<div class="border">
 					<div class="element">
-						<div class="title button clickable">
-						Thank you for your contributing to your community!<br>
+						<div class="title button clickable">Thank you for your contributing to your community!</div>
 						<div class="element_content">
 							<div class="table">
 								<div class="column100">
 									<div class="line">
+											<?php
+											$images = glob('images/fun/*');
+											$random_image = $images[rand(0, count($images) - 1)];
+											echo '<img src="'.$random_image.'"/>';
+											?>
 											<p>You are a valuable human being!<br>
-											Capable of so many (hopefully positive for everybody) things if you put your mind to it.</p>
+											Capable of so many (hopefully positive for everybody) things if you put your mind and love to it.</p>
 											<h2>YOU HAVE BEEN LOGGED OUT!
 											<br>
 											:)
